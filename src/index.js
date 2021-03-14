c1519d8cd8bc9edaae76dae76bddd7688d9faeb4
GeorgiyGubarkov <g.v.gubarkov@mpt.ru>
2021-03-14 12:36:49 +0300

    feat: human readble 0-999

--- src/index.js
+++ src/index.js
@@ -3,15 +3,24 @@ module.exports = function toReadable (number) {
       onse: ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve',
             'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'],
       tens: [null, null, 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'],
-      hadred: 'handred',
+      hadred: 'hundred',
   }
   if (number < 20) {
       return numbers.onse[number]
-  } else if (number < 100)  {
+  } else if (number < 100) {
       return number % 10 == 0 ? numbers.tens[Math.floor(number/10)] : `${numbers.tens[Math.floor(number/10)]} ${numbers.onse[number%10]}`
   } else {
-      console.log(`${numbers.onse[Math.floor(number/100)]} handred ${numbers.tens[Math.floor(number%100/10)]} ${numbers.onse[number%100%10]}`)
-      return `${numbers.onse[Math.floor(number/100)]} handred ${numbers.tens[Math.floor(number%100/10)]} ${numbers.onse[number%100%10]}`
+      if (number%100 == 0) {
+          return `${numbers.onse[number/100]} hundred`
+      } else if ((0<number%100) && (number%100<20)) {
+        console.log(`${numbers.onse[Math.floor(number/100)]} 1 hundred ${numbers.onse[number%100]}`)
+        return `${numbers.onse[Math.floor(number/100)]} hundred ${numbers.onse[number%100]}`
+      } else if ((number%10 == 0)) {
+          console.log(`${numbers.onse[Math.floor(number/100)]} 2 hundred ${numbers.tens[Math.floor(number%100/10)]}`)
+          return `${numbers.onse[Math.floor(number/100)]} hundred ${numbers.tens[Math.floor(number%100/10)]}`
+      } else {
+          console.log(`${numbers.onse[Math.floor(number/100)]} 3 hundred ${numbers.tens[Math.floor(number%100/10)]} ${numbers.onse[number%10]}`)
+          return `${numbers.onse[Math.floor(number/100)]} hundred ${numbers.tens[Math.floor(number%100/10)]} ${numbers.onse[number%10]}`
+      }
   }
-
 }
